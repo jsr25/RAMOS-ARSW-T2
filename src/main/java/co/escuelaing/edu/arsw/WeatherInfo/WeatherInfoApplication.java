a8505353ff34c538e453d8c9b2d02bc2b0ef9968
jsr25 <ramosjuans@live.com.ar>
2021-04-08 10:07:47 -0500

    first commit

---
+++ src/main/java/co/escuelaing/edu/arsw/WeatherInfo/WeatherInfoApplication.java
@@ -0,0 +1,13 @@
+package co.escuelaing.edu.arsw.WeatherInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/escuelaing/edu/arsw/WeatherInfo/WeatherInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.escuelaing.edu.arsw.WeatherInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
